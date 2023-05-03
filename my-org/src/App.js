09998ea19ac1070d0e24cc171e38fca6c2b17878
Paco Hdez <francj.hdez@gmail.com>
2023-05-02 19:01:39 -0600

    primer componente y developer tools

--- my-org/src/App.js
+++ my-org/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Hola mundo con React! 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
 
 export default App;
+
