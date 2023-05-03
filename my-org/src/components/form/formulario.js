0fb16d99b764d97c0b3d37b45a02a371fbfa71c4
Paco Hdez <francj.hdez@gmail.com>
2023-05-02 19:37:43 -0600

    componente formulario listo

---
+++ my-org/src/components/form/formulario.js
@@ -0,0 +1,11 @@
+import "./formulario.css"
+
+const Form = () => {
+    return <section className="form">
+        <form>
+            <h2>Rellena el formulario para crear el colaborador</h2>
+        </form>
+    </section>
+}
+
+export default Form
