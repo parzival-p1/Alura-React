6d5f1dd40a6371524784f3cd5b12a183f6e573df
Paco Hdez <francj.hdez@gmail.com>
2023-05-03 17:00:50 -0600

    manejando eventos onSubmit

--- my-org/src/components/form/formulario.js
+++ my-org/src/components/form/formulario.js
@@ -1,15 +1,25 @@
 import "./formulario.css"
 import CampoTxt from "../campoTxt"
 import ListaOpciones from "../listaOpciones"
+import Boton from "../boton"
 
 const Form = () => {
+
+    const manejarEnvio = (e) => {
+        e.preventDefault()
+        console.log("Manejar el envío", e);
+    }
+
     return <section className="form">
-        <form>
+        <form onSubmit={ manejarEnvio }>
             <h2>Rellena el formulario para crear el colaborador</h2>
             <CampoTxt titulo="Nombre" placeholder="Ingresar nombre"/>
             <CampoTxt titulo="Puesto" placeholder="Ingresar puesto"/>
             <CampoTxt titulo="Foto" placeholder="Ingresar enlace de foto"/>
             <ListaOpciones />
+            <Boton>
+                Crear
+            </Boton>
         </form>
     </section>
 }
