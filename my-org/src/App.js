d9a9a51afcf45ebd1914b71576e2e0cb5dc30a4e
Paco Hdez <francj.hdez@gmail.com>
2023-05-04 17:31:43 -0600

    estados de componentes utilizando el hook useState

--- my-org/src/App.js
+++ my-org/src/App.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/header/header.js';
 import Form from './components/form/formulario.js';
 import MiOrg from './components/miOrg';
 
 function App() {
+  const [mostrarForm, actualizarMostrar] = useState(true);
+
+   //^ Op Ternario --> condicion ? seMuestra : noSeMuestra
+  //^ condicion && seMuestra
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarForm) 
+  }
+
   return (
     <div>
       <Header />
-      <Form />
-      <MiOrg />
+      { mostrarForm === true ?  <Form /> : <> </> } {/* Fragments */} 
+     {/*  { mostrarForm && <Form /> } 2a op de ternario */}  
+      <MiOrg cambiarMostrar = { cambiarMostrar } />
 
     </div>
 
--- my-org/src/components/miOrg/index.js
+++ my-org/src/components/miOrg/index.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./miOrg.css"
 
-const MiOrg = () => {
+const MiOrg = (props) => {
+      //^ E s t a d o  -  h o o k s
+     //^ useState() function
+    //^ const [varName, updateFunc] = useState(initialValue)
+
+    console.log(props);
+/*     const [mostrar, actualizarMostrar] = useState(true);
+    const driveClick = () => {
+        console.log("Mostrar/Ocultar elemento", !mostrar);
+        actualizarMostrar(!mostrar);
+    }
+ */
     return <section className="orgSection">
         <h3 className="org-title">Mi organización</h3>
-        <img idName="org-img"  src="/img/add.png" alt="add"></img>
+        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar} />
     </section>
 }
 
