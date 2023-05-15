cd1689539094aba3fe2770265bcd8f7696028834
Paco Hdez <francj.hdez@gmail.com>
2023-05-15 15:24:39 -0600

    Crendo componentes: Error404 y Navbar

--- 3-React_Router-Nav_en_una_SPA/src/App.js
+++ 3-React_Router-Nav_en_una_SPA/src/App.js
@@ -2,6 +2,8 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home.jsx"
 import Sobre from "./pages/Sobre.jsx"
+import Page404 from './pages/Page404';
+import Header from './components/Header';
 
 import { BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 
@@ -11,9 +13,11 @@ function App() {
   //? Redireccion de rutas
   return (
     <Router>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
 
     </Router>
