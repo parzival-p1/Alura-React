cda4fd2826f42dfe368ced56ee4ea569dfdf043f
Paco Hdez <francj.hdez@gmail.com>
2023-05-16 20:50:33 -0600

    Adding API connection

---
+++ 3-React_Router-Nav_en_una_SPA/src/components/ListPost.jsx
@@ -0,0 +1,12 @@
+import "../assets/css/componentes/card.css"
+
+const ListPosts = ({ url }) => {
+
+    return (
+        <section className="posts container">
+
+        </section>
+    )
+}
+
+export default ListPosts
