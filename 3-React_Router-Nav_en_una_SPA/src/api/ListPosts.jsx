25c97fff34d8d317b94dee3bcb58e69f73da8903
Paco Hdez <francj.hdez@gmail.com>
2023-05-16 21:31:56 -0600

    moostrar los post con json db

--- 3-React_Router-Nav_en_una_SPA/src/api/ListPosts.jsx
+++ 3-React_Router-Nav_en_una_SPA/src/api/ListPosts.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react"
 import "../assets/css/componentes/card.css"
+import { busccar } from "../api/api.js"
+import { Link } from "react-router-dom"
 
 const ListPosts = ({ url }) => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        busccar(url, setPosts);
+    }, [url])
 
     return (
         <section className="posts container">
-
+            {
+                posts.map(post => {
+                    const { id, title, metadescription, categoria } = post;
+                    return <Link to={`/posts/${id}`} className={`post__card post-card--${categoria}`} key={id}>
+                        <article >
+                            <h3 className="post-card__title">
+                                {title}
+                            </h3>
+                            <p className="post-card__meta">{metadescription}</p>
+                        </article>
+                    </Link>
+                })
+            }
         </section>
     )
 }
