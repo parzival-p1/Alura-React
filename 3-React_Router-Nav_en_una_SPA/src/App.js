362d6780fd2d644ca7e22a9575bf7b3aba699c19
Paco Hdez <francj.hdez@gmail.com>
2023-05-18 11:57:03 -0600

    rutas anidadas y subcategorias

--- 3-React_Router-Nav_en_una_SPA/src/App.js
+++ 3-React_Router-Nav_en_una_SPA/src/App.js
@@ -17,13 +17,13 @@ function App() {
   return (
     <Router>
     <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sobre' element={<Sobre />} />
-        <Route path='/posts/:id' element={<Post />} />
-        <Route path='/categoria/:id' element={<Categoria />} />
-        <Route path='*' element={ <Page404 />} />
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sobre' element={<Sobre />} />
+      <Route path='/posts/:id' element={<Post />} />
+      <Route path='/categoria/:id/*' element={<Categoria />} />
+      <Route path='*' element={ <Page404 />} />
+    </Routes>
 
     </Router>
   );
